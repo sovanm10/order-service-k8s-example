1521236218c5447599a511d559904d40ef50d3e6
Sovan Mukherjee <sovmukh1@in.ibm.com>
2019-07-22 16:22:19 +0530

    updated OrderController

--- src/main/java/com/sm/controller/OrderController.java
+++ src/main/java/com/sm/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.sm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +17,18 @@ public class OrderController {
 	PropertiesConfig propertiesConfig;
 
 
+	@GetMapping("/")
+	public ResponseEntity<String> ping() {
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+	
 	@GetMapping("/order")
 	public String getOrders() {
 		log.info("<<<<OrderController>>>>>>>>>" + propertiesConfig.getDbUser());
 		return "db name from k8s config map:{"+propertiesConfig.getDbName()+"}, user name from secret: {"+propertiesConfig.getDbUser()+"}";
 	}
 	
-
+	
+	
 	
 }
